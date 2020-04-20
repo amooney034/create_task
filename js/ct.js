63dc4db9401fd01398c387880c8fcdcd92f43bcb
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-04-20 11:31:48 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -4,7 +4,23 @@ var ctx = canvas.getContext("2d");
 function gameBall() {
 	ctx.beginPath();
 	ctx.arc(175, 225, 10, 0, Math.PI*2);
-	ctx.fillStyle = white;
+	ctx.fillStyle = "white";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function paddleOne() {
+	ctx.beginPath();
+	ctx.rect(10, 175, 5, 50);
+	ctx.fillStyle = "white";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function paddleTwo() {
+	ctx.beginPath();
+	ctx.rect(440, 175, 5, 50);
+	ctx.fillStyle = "white";
 	ctx.fill();
 	ctx.closePath();
 }
