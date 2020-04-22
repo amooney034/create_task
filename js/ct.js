6794a987f94d549137e67cffb345ce5a22a2ad5b
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-04-22 10:35:56 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -1,26 +1,36 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-function gameBall() {
-	ctx.beginPath();
-	ctx.arc(175, 225, 10, 0, Math.PI*2);
-	ctx.fillStyle = "white";
-	ctx.fill();
-	ctx.closePath();
+	function gameball() {
+		ctx.beginPath();
+		ctx.arc(302, 200, 8, 0, Math.PI*2);
+		ctx.fillStyle = "white";
+		ctx.fill();
+		ctx.closePath();
 }
 
-function paddleOne() {
-	ctx.beginPath();
-	ctx.rect(10, 175, 5, 50);
-	ctx.fillStyle = "white";
-	ctx.fill();
-	ctx.closePath();
+
+	function paddleOne() {
+		ctx.beginPath();
+		ctx.rect(30, 175, 5, 50);
+		ctx.fillStyle = "white";
+		ctx.fill();
+		ctx.closePath();
+}
+	
+
+	function paddleTwo() {
+		ctx.beginPath();
+		ctx.rect(570, 175, 5, 50);
+		ctx.fillStyle = "white";
+		ctx.fill();
+		ctx.closePath();
 }
 
-function paddleTwo() {
-	ctx.beginPath();
-	ctx.rect(440, 175, 5, 50);
-	ctx.fillStyle = "white";
-	ctx.fill();
-	ctx.closePath();
+	function net() {
+		ctx.beginPath();
+		ctx.rect(300, 220, 5, 10);
+		ctx.fillStyle = "white";
+		ctx.fill();
+		ctx.closePath();
 }
