1de5ef871e60190e0910a7394c3dbddf0e6c9a9b
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-04-22 11:39:23 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -1,36 +1,42 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-	function gameball() {
+	function drawBall() {
 		ctx.beginPath();
-		ctx.arc(302, 200, 8, 0, Math.PI*2);
+		ctx.arc(303, 200, 8, 0, Math.PI*2);
 		ctx.fillStyle = "white";
 		ctx.fill();
 		ctx.closePath();
-}
+	}
 
 
-	function paddleOne() {
+	function drawPone() {
 		ctx.beginPath();
 		ctx.rect(30, 175, 5, 50);
 		ctx.fillStyle = "white";
 		ctx.fill();
 		ctx.closePath();
-}
+	}
 	
 
-	function paddleTwo() {
+	function drawPtwo() {
 		ctx.beginPath();
 		ctx.rect(570, 175, 5, 50);
 		ctx.fillStyle = "white";
 		ctx.fill();
 		ctx.closePath();
-}
+	}
 
-	function net() {
+
+	function drawNet() {
 		ctx.beginPath();
-		ctx.rect(300, 200, 5, 10);
+		ctx.rect(300, 220, 5, 10);
 		ctx.fillStyle = "white";
 		ctx.fill();
 		ctx.closePath();
 }
+
+drawBall();
+drawPone();
+drawPtwo();
+drawNet();
