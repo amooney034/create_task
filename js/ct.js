7b33e5c47eb6d627ebfdf71d49502b0d66d4f58f
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-05-04 10:35:48 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -1,44 +1,45 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-	function drawBall() {
-		ctx.beginPath();
-		ctx.arc(300, 200, 8, 0, Math.PI*2);
-		ctx.fillStyle = "white";
-		ctx.fill();
-		ctx.closePath();
-	}
+const startButton = function() {
+  if (startButton.onclick) {
+ctx.beginPath();
+ctx.arc(300, 200, 8, 0, Math.PI*2);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.closePath();
+ }
+}
+
+ function drawPone() {
+ctx.beginPath();
+ctx.rect(30, 175, 5, 50);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.closePath();
+}
 
 
-	function drawPone() {
-		ctx.beginPath();
-		ctx.rect(30, 175, 5, 50);
-		ctx.fillStyle = "white";
-		ctx.fill();
-		ctx.closePath();
-	}
-	
-
-	function drawPtwo() {
-		ctx.beginPath();
-		ctx.rect(570, 175, 5, 50);
-		ctx.fillStyle = "white";
-		ctx.fill();
-		ctx.closePath();
-	}
+ function drawPtwo() {
+ctx.beginPath();
+ctx.rect(570, 175, 5, 50);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.closePath();
+}
 
 
-	function drawNet() {
-		ctx.lineWidth = 5;
-		ctx.strokeStyle = "#FFFFFF";
-		ctx.beginPath();
-		ctx.setLineDash([20, 25]);
-		ctx.moveTo(300, 10);
-		ctx.lineTo(300, 590);
-		ctx.stroke();
-	}
+ function drawNet() {
+ctx.lineWidth = 5;
+ctx.strokeStyle = "#FFFFFF";
+ctx.beginPath();
+ctx.setLineDash([20, 25]);
+ctx.moveTo(300, 10);
+ctx.lineTo(300, 590);
+ctx.stroke();
+}
 
-drawBall();
 drawPone();
 drawPtwo();
 drawNet();
+startButton();
