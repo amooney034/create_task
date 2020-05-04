e6f07e6195c7c891235ce5867419144fbe17c537
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-05-04 17:32:07 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -1,1 +1,5 @@
-//RBG spectrum
+var canvas = document.getElementById("colorboard");
+var ctx = canvas.getContext("2d");
+var red = document.getElementById("red_input");
+var blue = document.getElementById("blue_input");
+var green = document.getElementById("green_input");
