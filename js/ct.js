30627d1b55a9573a43ded40c03290bb5c1f2be8b
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-05-06 10:44:36 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -1,8 +1,8 @@
 var min = 0;
 var max = 255; 
-var red_color = wndow.prompt("Enter the red value integer 0-255");
-var blue_color = wndow.prompt("Enter the blue value  integer 0-255");
-var green_color = wndow.prompt("Enter the green value integer 0-255");
+var red_color = window.prompt("Enter the red value integer 0-255");
+var blue_color = window.prompt("Enter the blue value  integer 0-255");
+var green_color = window.prompt("Enter the green value integer 0-255");
 
 if ((red_color > max) || (red_color < min) || (!Number.isInteger(red_color)) || (Number.isNaN (red_color))) {
   window.alert("Something is off with your red value.");
