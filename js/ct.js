4267253c5f8637dd1c78746500840e69c1862a5a
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-05-08 11:33:49 -0400

    Update ct.js

--- js/ct.js
+++ js/ct.js
@@ -39,7 +39,17 @@ var hex = parseInt(red_binary, 2).toString(16).toUpperCase();
 var hexa = parseInt(blue_binary, 2).toString(16).toUpperCase();
 var hexad = parseInt(green_binary, 2).toString(16).toUpperCase();
 
-var final_color = hex + hexa + hexad;
+if (red_color < 16) {
+  var red_final = 0+hex;
+}
+if (blue_color < 16) {
+  var blue_final = 0+hexa;
+}
+if (green_color < 16) {
+  var green_final = 0+hexad;
+}
+
+var final_color = red_final + blue_final + green_final;
 if ((red_color > max) || (red_color < min) || (red_color % 1 !==0) || (Number.isNaN (red_color)) || (blue_color > max) || (blue_color < min) || (blue_color % 1 !==0) || (Number.isNaN (blue_color)) || (green_color > max) || (green_color < min) || (green_color % 1 !=0) || (Number.isNaN (green_color))) {
   window.alert("Invalid inputs.");
   }
